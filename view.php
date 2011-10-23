6eb3fdf24ab075890ee45e057ddc6f2171b2864b
Dennis Iversen <dennis.iversen@gmail.com>
2011-10-23 18:02:43 +0200

    easier to integrate in other modules.

--- view.php
+++ view.php
@@ -1,32 +1,3 @@
 <?php
 
-/**
- * view file for gallery
- *
- * @package    gallery
- */
-$galImg = new gallery(true);
-
-template::setTitle(lang::translate('View gallery'));
-if (isset($_POST['submit'])){
-
-    if (!session::checkAccessControl('gallery_allow_edit')){
-        return;
-    }
-
-    $galImg->validateInsert();
-    if (!isset($galImg->errors)){
-        $res = $galImg->insertFile('filename');
-        if ($res){
-            session::setActionMessage(lang::translate('gallery_file_added'));
-            header("Location: $_SERVER[REQUEST_URI]");
-        }
-    } else {
-        view_form_errors($galImg->errors);
-    }
-}
-
-$options = array ('gallery_id' => $galImg->id);
-$files = $galImg->getAllFiles();
-$info = $galImg->getAllFileInfo($galImg->id);
-echo $galImg->getThumbTableHTML($info, $options);
+gallery::viewGallery();
