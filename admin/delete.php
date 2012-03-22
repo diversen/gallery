567028e60f4accd9cfe8c49086d1c8cd7324c3d2
dennis <dennis@dennis-Rev-1-0.(none)>
2012-03-22 18:37:44 +0100

    fix lang. Add delete gallery

--- admin/delete.php
+++ admin/delete.php
@@ -6,15 +6,25 @@ if (!session::checkAccessControl('gallery_allow_edit')){
 
 template::setTitle(lang::translate('Delete Gallery'));
 $gallery = new galleryAdmin();
+$row = $gallery->getGallery();
+//print_r($row);
+
+$gal = new gallery();
+$rows = $gal->getAllFileInfo($row['id']);
+
+if (!empty($rows)) {
+    echo lang::translate('gallery_delete_has_files');
+    return;
+}
+
+
 if (!empty($_POST['submit'])){
-    $res = $gallery->deleteGallery();
+    $res = $gallery->deleteGallery($row['id']);
     if ($res){
         session::setActionMessage(
-            lang::translate("Gallery") . " " .
-            lang::translate('has been deleted'));
-        header("Location: /gallery/admin/index");
+            lang::translate('gallery_gallery_has_been_deleted'));
+        header("Location: /gallery/index");
     }
-} else {
-    $row = $gallery->getGallery();
-    view_gallery_form('delete');
+} else { 
+    view_gallery_form('delete', $row['id']);
 }
