d42f6c22172adb57f2549eaac10a45c9c30a63e2
dennis <dennis@dennis-Rev-1-0.(none)>
2012-08-02 13:36:56 +0200

    clean up

--- admin/delete.php
+++ admin/delete.php
@@ -7,17 +7,6 @@ if (!session::checkAccessControl('gallery_allow_edit')){
 template::setTitle(lang::translate('Delete Gallery'));
 $gallery = new galleryAdmin();
 $row = $gallery->getGallery();
-//print_r($row);
-
-$gal = new gallery();
-/*
-$rows = $gal->getAllFileInfo($row['id']);
-
-if (!empty($rows)) {
-    echo lang::translate('gallery_delete_has_files');
-    return;
-}*/
-
 
 if (!empty($_POST['submit'])){
     $res = $gallery->deleteGallery($row['id']);
